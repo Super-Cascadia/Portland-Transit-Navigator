38544c36be958b0af9221898b2331c052d7051d3
Jameson Nyeholt <jnyeholt@snapflow.com>
2014-05-06 08:10:36 -0700

    adds route schedule to git

---
+++ app/scripts/controllers/routeSchedule.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('pdxStreetcarApp')
+    .controller('RouteScheduleCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
