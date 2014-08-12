52a8950015ceb1e634be193263433a40c0ecabd0
Jameson Nyeholt <jameson.nyeholt@gmail.com>
2014-08-12 02:19:46 -0700

    deletions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,31 +28,6 @@ angular.module('pdxStreetcarApp', [
                 url: '/about',
                 templateUrl: 'views/about/about.html',
                 controller: 'AboutCtrl as about'
-            })
-            .state('arrivals', {
-                url: '/arrivals',
-                templateUrl: 'views/arrivalsView.html',
-                controller: 'ArrivalsMainCtrl'
-            })
-            .state('arrivals.route', {
-                url: '/:route',
-                templateUrl: 'views/transitTimeView/partials/primaryView.html',
-                controller: 'PrimaryViewCtrl'
-            })
-            .state('arrivals.route.direction', {
-                url: '/:direction',
-                templateUrl: 'views/transitTimeView/partials/leftColumn.html',
-                controller: 'RouteDirectionCtrl'
-            })
-            .state('arrivals.route.direction.stop', {
-                url: '/:stop',
-                controller: 'StopCtrl',
-                templateUrl: 'views/transitTimeView/partials/stopSelector.html'
-            })
-            .state('routeSchedule', {
-                url: '/routeSchedule',
-                templateUrl: 'views/routeSchedule/routeSchedule.html',
-                controller: 'RouteScheduleCtrl'
             });
     })
 
