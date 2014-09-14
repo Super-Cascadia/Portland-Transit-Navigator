eacd5ffa290e772bf87028fa125ca091b9ca55e0
Jameson Nyeholt <jameson.nyeholt@gmail.com>
2014-09-14 09:50:24 -0700

    initial unit tests

---
+++ test/spec/controllers/services.js
@@ -0,0 +1,63 @@
+'use strict';
+describe('Factory: trimetUtilities', function () {
+
+  var trimetUtilities;
+
+  beforeEach(function () {
+    module('pdxStreetcarApp');
+  });
+
+  beforeEach(inject(function (_trimetUtilities_) {
+    trimetUtilities = _trimetUtilities_;
+  }));
+
+
+  describe('#trimetUtilities.isStreetCarRoute', function () {
+
+    describe('called with a valid set of data', function () {
+
+      it('should return true', inject(function (trimetUtilities) {
+
+        var arrival = {};
+
+        arrival.route = 193;
+
+        var result = trimetUtilities.isStreetCarRoute(arrival);
+
+        expect(result).toBe(true);
+
+      }));
+
+      it('should return true', inject(function (trimetUtilities) {
+
+        var arrival = {};
+
+        arrival.route = 194;
+
+        var result = trimetUtilities.isStreetCarRoute(arrival);
+
+        expect(result).toBe(true);
+
+      }));
+
+    });
+
+    describe('called with a invalid set of data', function () {
+
+      it('should return true', inject(function (trimetUtilities) {
+
+        var arrival = {};
+
+        arrival.route = 400;
+
+        var result = trimetUtilities.isStreetCarRoute(arrival);
+
+        expect(result).toBe(false);
+
+      }));
+
+    });
+
+  });
+
+});
